96615cdfd9f81ef774f0e41895a9ab526f78a9fc
johaneris <jsavalos@uamv.edu.ni>
2025-06-29 22:59:13 -0600

    Subiendo el documento e inicio del analisis de merge

---
+++ prueba.py
@@ -0,0 +1,18 @@
+import random 
+import time
+import tracemalloc
+import pandas
+import matplotlib
+
+
+#implementar los algoritmos de ordenamiento
+
+def mergesort(lista):
+    if len(lista) <= 1:
+        return lista
+    
+
+        mid = len(lista) //2
+        
+
+
